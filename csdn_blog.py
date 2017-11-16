223aa5976e36cb94c18f8e4078d9de44bfa421d9
jochen <942814208@qq.com>
2017-11-16 14:14:39 +0800

    readme

---
+++ csdn_blog.py
@@ -0,0 +1,29 @@
+#!/usr/bin/Python
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Created on 2017-11-16 13:15:16
+# Project: csdn_blog
+
+from pyspider.libs.base_handler import *
+
+
+class Handler(BaseHandler):
+    crawl_config = {
+    }
+
+    @every(minutes=24 * 60)
+    def on_start(self):
+        self.crawl('http://blog.csdn.net/mishifangxiangdefeng', callback=self.index_page)
+
+    @config(age=10 * 24 * 60 * 60)
+    def index_page(self, response):
+        for each in response.doc('a[href^="http"]').items():
+            self.crawl(each.attr.href, callback=self.detail_page)
+
+    @config(priority=2)
+    def detail_page(self, response):
+        return {
+            "url": response.url,
+            "title": response.doc('title').text(),
+        }
